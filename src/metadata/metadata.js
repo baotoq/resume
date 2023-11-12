2c2e754337e694577721f9dc5e01927535ab322b
Bao To Quoc <baotoq@outlook.com>
2023-11-12 20:02:45 +0700

    resume

---
+++ src/metadata/metadata.js
@@ -0,0 +1,78 @@
+module.exports = {
+  name: "To Quoc Bao",
+  title: "Software Engineer",
+  baseUrl: "",
+  facts: {
+    LinkedIn:
+      '<a href="https://www.linkedin.com/in/baotoq"><i class="fab fa-linkedin fact-icon"></i>baotoq</a>',
+    GitHub:
+      '<a href="https://github.com/baotoq"><i class="fab fa-github fact-icon"></i>baotoq</a>',
+    Email: '<a href="mailto:baotoq@outlook.com">baotoq@outlook.com</a>',
+  },
+  skills: [
+    ["JS/TS", "+++"],
+    ["Golang", "+++"],
+    ["CI/CD", "+++"],
+    ["SQL", "+++"],
+    ["Kubernetes", "++"],
+    ["ELK", "++"],
+    ["AWS", "++"],
+    ["GCP", "+"],
+  ],
+  summary: `
+    <div>Have 5 years of experience working in software development industry.</div>
+    <div>Experience in using Microsoft technologies, especially .NET Core and Azure to develop RESTful APIs and gRPC services.</div>
+    <div>Familiar with micro-services architecture. Knowledge of Docker, Kubernetes, CI/CD and GitOps.</div>
+    `,
+  positions: [
+    {
+      company: "Upmesh",
+      link: "https://www.upmesh.io",
+      location: "",
+      title: "Software Engineer",
+      period: "Oct 2021 - Now",
+      skills: [
+        "ASP.NET Core",
+        "Golang",
+        "gRPC",
+        "ELK Stack",
+        "Kubenetes",
+        "AWS",
+        "Terraform",
+        "FluxCD",
+      ],
+      contents: `
+hello world
+`,
+    },
+    {
+      company: "AS White Global",
+      link: "",
+      location: "",
+      title: "Software Engineer",
+      period: "Jan 2021 - Sep 2021",
+      skills: ["NodeJS", "ReactJS", "Kubenetes", "AWS", "GCP"],
+      contents: `
+asasdas
+`,
+    },
+    {
+      company: "NashTech Limited",
+      link: "",
+      location: "",
+      title: "Software Engineer",
+      period: "Dec 2018 - Dec 2020",
+      skills: ["NodeJS", "ReactJS", "ELK Stack", "Kubenetes"],
+      contents: `
+dsvdfvdfv
+`,
+    },
+  ],
+  education: [
+    {
+      period: "2014 &ndash; 2018",
+      contents: `<b>Bachelor of Computer Science</b> at Ton Duc Thang University`,
+    },
+  ],
+  others: [],
+};
