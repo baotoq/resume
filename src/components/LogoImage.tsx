b15f2366c764119bb3239241785637e85fd5de8c
Bao To <baotoq@outlook.com>
2026-04-22 17:07:01 +0700

    style(08-01): fix import order in LogoImage.tsx per biome organizeImports
    - Reorder imports: next/image before react (alphabetical)
    - Pre-existing lint issue in file modified by this plan

--- src/components/LogoImage.tsx
+++ src/components/LogoImage.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useState } from "react";
 import Image from "next/image";
+import { useState } from "react";
 
 interface LogoImageProps extends React.HTMLAttributes<HTMLDivElement> {
   src: string | undefined;
