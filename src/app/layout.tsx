0d0c441537c10b99d00d79aa696b66ab68d8a1bd
Bao To <baotoq@outlook.com>
2026-04-29 16:26:28 +0700

    up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -68,7 +68,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         {children}
-        <SpeedInsights />
+        {process.env.VERCEL && <SpeedInsights />}
       </body>
     </html>
   );
